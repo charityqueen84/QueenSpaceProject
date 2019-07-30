30e3259bc3116a9757a39e29ed63c003938a5956
Charity <charityqueen@gmail.com>
2019-07-30 17:53:54 -0500

    Creating repo

---
+++ src/main/java/com/example/QueenSpaceProject/QueenSpaceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.QueenSpaceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueenSpaceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueenSpaceProjectApplication.class, args);
+	}
+
+}
