f3e2eb7e81afbe7832f898bf55311b561eebaebd
Charity <charityqueen@gmail.com>
2019-07-30 23:06:44 -0500

    moved css to static and fixed background image. Added /space page

--- src/main/java/com/example/QueenSpaceProject/controllers/SpaceController.java
+++ src/main/java/com/example/QueenSpaceProject/controllers/SpaceController.java
@@ -3,11 +3,21 @@ package com.example.QueenSpaceProject.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
 @Controller
 public class SpaceController {
+
     @RequestMapping(value = " ")
     public String index() {
         return "index";
+
     }
+    @RequestMapping(value = "space")
+    public String space() {
+        return "space";
+    }
+
+
 
 }
