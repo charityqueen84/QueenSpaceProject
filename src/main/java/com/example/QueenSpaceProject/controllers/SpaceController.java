5145c3cdd9215df5b3d89109ff06736ae2e6691e
Charity <charityqueen@gmail.com>
2019-08-04 15:59:17 -0500

    Added another fact to the ArrayList and changed facts.html to loop over the list and display those facts instead of the text

--- src/main/java/com/example/QueenSpaceProject/controllers/SpaceController.java
+++ src/main/java/com/example/QueenSpaceProject/controllers/SpaceController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 public class SpaceController {
+
+    private static List<String> facts;
+    public SpaceController() {
+        facts = new ArrayList<String>();
+        facts.add("Giant, spinning storms on Neptune are big enough to swallow the whole Earth.");
+        facts.add("Saturn is the only planet in our solar system that is less\n" +
+                "dense than water. It could float in a bathtub.");
+    }
 
     @RequestMapping(value = " ")
     public String index() {
@@ -20,7 +30,7 @@ public class SpaceController {
     }
     @RequestMapping(value = "display")
     public String displayFacts(Model model) {
-
+        model.addAttribute("facts", facts);
         model.addAttribute("count", 5);
         return "facts";
     }
