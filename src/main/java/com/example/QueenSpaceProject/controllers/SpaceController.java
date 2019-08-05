ddb0aff4e990f204153a7ea91d20b8f6efa27954
Charity <charityqueen@gmail.com>
2019-08-04 19:22:41 -0500

    added to controller and facts.html so facts could be added to the list

--- src/main/java/com/example/QueenSpaceProject/controllers/SpaceController.java
+++ src/main/java/com/example/QueenSpaceProject/controllers/SpaceController.java
@@ -1,8 +1,10 @@
 package com.example.QueenSpaceProject.controllers;
 
+import com.example.QueenSpaceProject.models.Facts;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +13,14 @@ import java.util.List;
 @Controller
 public class SpaceController {
 
-    private static List<String> facts;
+    private static List<Facts> facts;
+
     public SpaceController() {
-        facts = new ArrayList<String>();
-        facts.add("Giant, spinning storms on Neptune are big enough to swallow the whole Earth.");
-        facts.add("Saturn is the only planet in our solar system that is less\n" +
-                "dense than water. It could float in a bathtub.");
+        facts = new ArrayList<Facts>();
+        facts.add(new Facts("Neptune’s\n" +
+                "giant, spinning storms could swallow the whole Earth. "));
+        facts.add(new Facts("Saturn is the only planet in our solar system that is less\n" +
+                "dense than water. It could float in a bathtub."));
     }
 
     @RequestMapping(value = " ")
@@ -24,17 +28,24 @@ public class SpaceController {
         return "index";
 
     }
+
     @RequestMapping(value = "space")
     public String space() {
         return "space";
     }
+
     @RequestMapping(value = "display")
     public String displayFacts(Model model) {
+        System.out.println("Loading space facts");
         model.addAttribute("facts", facts);
         model.addAttribute("count", 5);
         return "facts";
     }
 
-
-
+    @RequestMapping(value = "new")
+    public String addFact(Model model, @RequestParam String spaceFactField) {
+        System.out.println("Adding space facts: " + spaceFactField);
+        facts.add(new Facts(spaceFactField));
+        return "redirect:";
+    }
 }
--- src/main/java/com/example/QueenSpaceProject/models/Facts.java
+++ src/main/java/com/example/QueenSpaceProject/models/Facts.java
@@ -1,12 +1,11 @@
 package com.example.QueenSpaceProject.models;
 
 public class Facts {
-    private String fact;
+    private String fact; //add source category later? So I can name my info source (Ex: NASA)
 
     public Facts(String fact) {
         this.fact = fact;
     }
-
     public String getFact() {
         return fact;
     }
